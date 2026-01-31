b28fad830107c0e20de8b3aa6d83a3f22ef0842b
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-31 09:10:17 +0530

    added mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
@@ -14,17 +15,17 @@ fi
 
 mkdir -p $LOGS_FOLDER
 
-
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$R ...$R FAILURE $N"|tee -a $LOGS_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-       echo -e "$G ...$G SUCCESS $N"|tee -a $LOGS_FILE
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
+
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Copying Mongo Repo"
+VALIDATE $? "Copying Mongo Repo" 
 
 dnf install mongodb-org -y &>>$LOGS_FILE
 VALIDATE $? "Installing MongoDB server"
@@ -32,8 +33,8 @@ VALIDATE $? "Installing MongoDB server"
 systemctl enable mongod &>>$LOGS_FILE
 VALIDATE $? "Enable MongoDB"
 
-systemctl start mongod &>>$LOGS_FILE
-VALIDATE $? "Starting MongoDB"
+systemctl start mongod
+VALIDATE $? "Start MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections"
